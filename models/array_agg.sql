f6fc57bb99e7f917f456defe0e95013a56f81c52
Kristin Bagnall <54283769+fivetran-kristin@users.noreply.github.com>
2020-04-01 14:25:00 -0700

    Create array_agg model file

---
+++ models/array_agg.sql
@@ -0,0 +1,15 @@
+{% macro array_agg(field_to_agg, order_by) -%}
+
+{{ adapter_macro('netsuite.array_agg') }}
+
+{%- endmacro %}
+
+{% macro default__array_agg(field_to_agg, order_by) %}
+    array_agg({{ field_to_agg }} order by {{ order_by }})
+
+{% endmacro %}
+
+{% macro snowflake__array_agg(field_to_agg, order_by) %}
+    array_agg({{ field_to_agg }}) within group (order by {{ order_by }})
+
+{% endmacro %}
