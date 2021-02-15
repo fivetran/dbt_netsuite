d694c8bcb1fdf59b2ad4481aaa3bbc1b3f39ae6d
Matthieu Di Mercurio <matthieu.dimercurio@dialogue.co>
2021-02-15 14:55:20 -0500

    feat: add extra info to income statement (#23)
    * feat: add subsidiary info to income statement
    
    * add parent class

--- models/income_statement.sql
+++ models/income_statement.sql
@@ -36,6 +36,7 @@ select
   accounts.accountnumber as account_number,
   {{ dbt_utils.concat(['accounts.accountnumber',"'-'", 'accounts.name']) }} as account_number_and_name,
   classes.full_name as class_full_name,
+  coalesce(parent_class.full_name, classes.full_name) as parent_class_full_name,
   locations.full_name as location_full_name,
   departments.full_name as department_full_name,
   -converted_amount_using_transaction_accounting_period as converted_amount,
@@ -46,16 +47,21 @@ select
     when lower(accounts.type_name) = 'other income' then 4
     when lower(accounts.type_name) = 'other expense' then 5
     else null
-    end as income_statement_sort_helper
+    end as income_statement_sort_helper,
+  subsidiaries.subsidiary_id,
+  subsidiaries.full_name as subsidiary_full_name,
+  subsidiaries.name as subsidiary_name
 from transactions_with_converted_amounts
 join transaction_lines as transaction_lines
   on transaction_lines.transaction_line_id = transactions_with_converted_amounts.transaction_line_id
   and transaction_lines.transaction_id = transactions_with_converted_amounts.transaction_id
 left join classes on classes.class_id = transaction_lines.class_id
+left join classes as parent_class on parent_class.class_id = classes.parent_id
 left join locations on locations.location_id = transaction_lines.location_id
 left join departments on departments.department_id = transaction_lines.department_id
 join accounts on accounts.account_id = transactions_with_converted_amounts.account_id
 join accounting_periods as reporting_accounting_periods on reporting_accounting_periods.accounting_period_id = transactions_with_converted_amounts.reporting_accounting_period_id
+join subsidiaries on transactions_with_converted_amounts.subsidiary_id = subsidiaries.subsidiary_id
 where reporting_accounting_periods.fiscal_calendar_id  = (select fiscal_calendar_id from subsidiaries where parent_id is null)
   and transactions_with_converted_amounts.transaction_accounting_period_id = transactions_with_converted_amounts.reporting_accounting_period_id
   and transactions_with_converted_amounts.is_income_statement
