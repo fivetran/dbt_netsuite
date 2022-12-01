0693548d916538beeb8dcfebe911277993f36b75
fivetran-joemarkiewicz <joseph.markiewicz@fivetran.com>
2022-12-01 16:15:27 -0600

    bugfix/netsuite2-income-statement

--- models/netsuite2/intermediate/int_netsuite2__tran_lines_w_accounting_period.sql
+++ models/netsuite2/intermediate/int_netsuite2__tran_lines_w_accounting_period.sql
@@ -23,7 +23,7 @@ transaction_lines_w_accounting_period as ( -- transaction line totals, by accoun
   join transactions on transactions.transaction_id = transaction_lines.transaction_id
 
   where lower(transactions.transaction_type) != 'revenue arrangement'
-    and not transaction_lines.is_posting
+    and transaction_lines.is_posting
 )
 
 select * 
