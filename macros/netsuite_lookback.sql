bfee8ee16b924d611a9feb52cba3899a7afd8200
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2024-04-10 15:15:52 -0500

    update macro

--- macros/netsuite_lookback.sql
+++ macros/netsuite_lookback.sql
@@ -6,6 +6,15 @@
 
 {% macro default__netsuite_lookback(from_date, datepart, interval, safety_date='2010-01-01')  %}
 
+    coalesce(
+        (select {{ dbt.dateadd(datepart=datepart, interval=-interval, from_date_or_timestamp=from_date) }} 
+            from {{ this }}), 
+        {{ "'" ~ safety_date ~ "'" }}
+        )
+
+{% endmacro %}
+
+{% macro bigquery__mixpanel_lookback(from_date, datepart, interval, safety_date='2010-01-01')  %}
     {% set sql_statement %}
         select coalesce({{ from_date }}, {{ "'" ~ safety_date ~ "'" }})
         from {{ this }}
