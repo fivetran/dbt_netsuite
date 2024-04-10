dae8d3ef5d2016d7648fbb8de4cfb242eaa12b6a
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2024-04-10 14:06:31 -0500

    update macro

--- macros/netsuite_lookback.sql
+++ macros/netsuite_lookback.sql
@@ -8,12 +8,13 @@
 
     -- Capture the latest timestamp in a call statement instead of a subquery for optimizing BQ costs on incremental runs
     {% set sql_statement %}
-        select {{ from_date }} from {{ this }}
+        select coalesce({{ from_date }}, {{ "'" ~ safety_date ~ "'" }})
+        from {{ this }}
     {%- endset -%}
 
     -- the query_result is stored as a dataframe. Therefore, we want to now store it as a singular value.
-    {%- set result = dbt_utils.get_single_value(sql_statement, default=safety_date) -%}
+    {%- set result = dbt_utils.get_single_value(sql_statement) -%}
 
-    {{ dbt.dateadd(datepart=datepart, interval=-interval, from_date_or_timestamp="'" ~ result ~ "'") }}
+    {{ dbt.dateadd(datepart=datepart, interval=-interval, from_date_or_timestamp="cast('" ~ result ~ "' as date)") }}
 
 {% endmacro %}
