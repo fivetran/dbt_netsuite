33d56050b29424e1419b2c6f364bff1cc3a0388b
Joe Markiewicz <74217849+fivetran-joemarkiewicz@users.noreply.github.com>
2025-03-25 17:51:37 -0500

    feature/netsuite2-fiscal-year-end

--- models/netsuite2/intermediate/base/int_netsuite2__accounting_periods.sql
+++ models/netsuite2/intermediate/base/int_netsuite2__accounting_periods.sql
@@ -1,27 +1,53 @@
 {{ config(enabled=var('netsuite_data_model', 'netsuite') == var('netsuite_data_model_override','netsuite2')) }}
 
 with accounting_periods as (
-
-    select *
-    from {{ var('netsuite2_accounting_periods') }}
+    select * from {{ var('netsuite2_accounting_periods') }}
 ),
 
 accounting_period_fiscal_calendars as (
+    select * from {{ var('netsuite2_accounting_period_fiscal_calendars') }}
+),
 
-    select *
-    from {{ var('netsuite2_accounting_period_fiscal_calendars') }}
+fiscal_calendar as (
+    select * from {{ var('netsuite2_fiscal_calendar') }}
 ),
 
 joined as (
-
     select 
         accounting_periods.*,
-        accounting_period_fiscal_calendars.fiscal_calendar_id
-
+        accounting_period_fiscal_calendars.fiscal_calendar_id,
+        fiscal_calendar.fiscal_month
     from accounting_periods
     left join accounting_period_fiscal_calendars
         on accounting_periods.accounting_period_id = accounting_period_fiscal_calendars.accounting_period_id
+    left join fiscal_calendar
+        on fiscal_calendar.fiscal_calendar_id = accounting_period_fiscal_calendars.fiscal_calendar_id
+),
+
+year_extract as (
+    select 
+        joined.*,
+        {{ get_month_number("fiscal_month") }} as fiscal_start_month,
+        extract(year from joined.starting_at) as calendar_year
+    from joined
+),
+
+adjusted as (
+    select 
+        *,
+        case 
+            when extract(month from starting_at) < fiscal_start_month then calendar_year - 1
+            else calendar_year
+        end as fiscal_year_start
+    from year_extract
+),
+
+final as (
+    select
+        *,
+        {{ date_from_parts('fiscal_year_start', 'fiscal_start_month', '1') }} as fiscal_year_trunc
+    from adjusted
 )
 
 select *
-from joined
+from final
