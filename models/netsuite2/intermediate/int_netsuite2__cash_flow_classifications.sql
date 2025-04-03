54aafdd1af4b3d423bf2bc108168acc2de97476c
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2025-04-02 20:50:34 -0500

    feature/cash-flow-report

---
+++ models/netsuite2/intermediate/int_netsuite2__cash_flow_classifications.sql
@@ -0,0 +1,49 @@
+{{ config(
+  enabled = var('netsuite_data_model', 'netsuite') == var('netsuite_data_model_override', 'netsuite2')
+) }}
+
+-- load the dictionary with the classification filters
+{% set classifications = var('cash_flow_classifications', var('cash_flow_defaults', {})) %}
+
+with transaction_details as (
+  select *
+  from {{ ref('netsuite2__transaction_details') }}
+), 
+
+transaction_classifications as (
+    select
+        *,
+        -- iterate through the categories and filters in classifications
+        case
+          {% for category, filters in classifications.items() %}
+            {% for filter in filters %}
+              when {{ filter.condition }}
+              {% if filter.exclude is defined and filter.exclude %}
+                then null
+              {% else %}
+                then '{{ category }}'
+              {% endif %}
+            {% endfor %}
+          {% endfor %}
+          else null
+          end as cash_flow_category,
+
+        case
+          {% for category, filters in classifications.items() %}
+            {% for filter in filters %}
+              when {{ filter.condition }}
+              {% if filter.subcategory is defined  %}
+                then '{{ filter.subcategory }}'
+              {% else %}
+                then null
+              {% endif %}
+            {% endfor %}
+          {% endfor %}
+          else null
+          end as cash_flow_subcategory
+    from transaction_details
+)
+
+select *
+from transaction_classifications
+where cash_flow_category is not null
--- models/netsuite2/netsuite2__cash_flow.sql
+++ models/netsuite2/netsuite2__cash_flow.sql
@@ -1,6 +1,8 @@
-{{ config(enabled=var('netsuite_data_model', 'netsuite') == var('netsuite_data_model_override','netsuite2'))}}
+{{ config(
+    enabled = var('netsuite_data_model', 'netsuite') == var('netsuite_data_model_override', 'netsuite2')
+) }}
 
-{%- set multibook_enabled = true if var('netsuite2__multibook_accounting_enabled', false) -%}
+{%- set multibook_enabled = var('netsuite2__multibook_accounting_enabled', false) -%}
 {%- set multibook_cols = ['accounting_book_id', 'accounting_book_name'] -%}
 
 {%- set to_subsidiary_enabled = (var('netsuite2__using_to_subsidiary', false) and var('netsuite2__using_exchange_rate', true)) -%}
@@ -11,127 +13,41 @@
 {%- do base_cols_list.extend(to_subsidiary_cols) if to_subsidiary_enabled -%}
 {%- set base_cols_sql = base_cols_list | join(',\n') -%}
 
-with income_statement as (
-    select
-        {{ base_cols_sql }},
-        sum(case when lower(account_category) in ('income', 'expense') then transaction_amount else 0 end) as net_income,
-        sum(case when lower(account_name) like '%depreciation%' or lower(account_name) like '%amortization%' then transaction_amount else 0 end) as non_cash_expenses
-    from {{ ref('netsuite2__income_statement') }}
-    {{ dbt_utils.group_by(base_cols_list|length) }}
+with cash_flow_classifications as (
+    select *
+    from {{ ref('int_netsuite2__cash_flow_classifications') }}
 ),
 
-balance_sheet as (
+aggregated_transactions as (
     select
         {{ base_cols_sql }},
-        account_type_name,
-        account_type_id,
-        account_name,
-        account_id,
-        sum(transaction_amount) as transaction_amount
-    from {{ ref('netsuite2__balance_sheet') }}
-    where lower(account_type_id) in ('acctpay', 'acctrec', 'bank', 'credcard', 
-        'deferexpense', 'deferrevenue', 'equity', 'fixedasset', 'longtermliab', 
-        'othasset', 'othcurrasset', 'othcurrliab', 'unbilledrec')
-    and not is_accounting_period_adjustment
-    {{ dbt_utils.group_by(base_cols_list|length + 4) }}
-),
+        cash_flow_category,
+        sum(transaction_amount) as cash_ending_period
+    from cash_flow_classifications
+    {{ dbt_utils.group_by(base_cols_list|length + 1) }}
+), 
 
-balance_sheet_prev_transaction as (
+with_lag as (
     select
         *,
-        lag(transaction_amount) over (
-            partition by -- need to make these dynamic
-                {{ 'accounting_book_id,' if multibook_enabled }}
-                {{ 'to_subsidiary_id,' if to_subsidiary_enabled }}
-                subsidiary_id,
-                account_id
+        lag(cash_ending_period) over (
+            partition by
+                {{ base_cols_sql }},
+                cash_flow_category
             order by accounting_period_ending
-        ) as prev_transaction_amount
-    from balance_sheet
-),
-
-balance_sheet_changes as (
-    select
-        *,
-        transaction_amount - coalesce(prev_transaction_amount, 0) as change_since_prev_period
-    from balance_sheet_prev_transaction
-),
-
-category_changes as (
-    select
-        {{ base_cols_sql }},
-        -- working capital changes 
-        sum(case when lower(account_type_id) = 'acctrec' 
-            then change_since_prev_period else 0 end) as change_in_ar,
-        sum(case when lower(account_type_id) = 'acctpay' 
-            then change_since_prev_period else 0 end) as change_in_ap,
-        sum(case when lower(account_type_id) = 'othcurrasset' 
-            then change_since_prev_period else 0 end) as change_in_inventory,
-
-        -- investment changes
-        sum(case when lower(account_type_id) in ('deferexpense', 'fixedasset', 'othasset', 'unbilledrec')
-            then change_since_prev_period else 0 end) as change_in_investments,
-
-        -- financing changes
-        sum(case when lower(account_type_id) = 'equity' and lower(account_name) not like '%stock%' -- stocks not treated as cash
-            then change_since_prev_period else 0 end) as change_in_equity,
-        sum(case when lower(account_type_id) = 'deferrevenue' 
-            then change_since_prev_period else 0 end) as change_in_deferred_revenue,
-        sum(case when lower(account_type_id) in ('longtermliab', 'othcurrliab', 'credcard') 
-            then change_since_prev_period else 0 end) as change_in_debt,
-
-        -- bank changes
-        sum(case when lower(account_type_id) = 'bank'
-            then change_since_prev_period else 0 end) as change_in_cash
-    from balance_sheet_changes
-    {{ dbt_utils.group_by(base_cols_list|length) }}
-),
-
-cash_flow as (
-    select
-        {% for col in base_cols_list %}
-        income_statement.{{ col }},
-        {% endfor %}
-
-        sum(coalesce(income_statement.net_income, 0)) as net_income,
-        sum(coalesce(income_statement.non_cash_expenses, 0)) as non_cash_expenses,
-
-        -- Operating Cash Flow
-        sum(coalesce(category_changes.change_in_ar, 0)) as change_in_ar,
-        sum(coalesce(category_changes.change_in_ap, 0)) as change_in_ap,
-        sum(coalesce(category_changes.change_in_inventory, 0)) as change_in_inventory,
-        sum((income_statement.net_income
-            + income_statement.non_cash_expenses
-            - coalesce(category_changes.change_in_ar, 0)
-            - coalesce(category_changes.change_in_inventory, 0)
-            + coalesce(category_changes.change_in_ap, 0))) as cash_from_operations,
-
-        -- Investing Cash Flow
-        sum(coalesce(category_changes.change_in_investments, 0)) as cash_from_investing,
-
-        -- Financing Cash Flow
-        sum(coalesce(category_changes.change_in_equity, 0) 
-            + coalesce(category_changes.change_in_deferred_revenue, 0) 
-            + coalesce(category_changes.change_in_debt, 0)) as cash_from_financing,
-
-        -- Bank Change in Cash
-        sum(coalesce(category_changes.change_in_cash, 0)) as reported_change_in_cash
-
-    from income_statement
-    left join category_changes
-        on 
-        {% for col in base_cols_list %}
-            {{ 'and' if not loop.first}} income_statement.{{ col }} = category_changes.{{ col }}
-        {% endfor %}
-    {{ dbt_utils.group_by(base_cols_list|length) }}
+        ) as cash_beginning_period
+    from aggregated_transactions
 ),
 
 final as (
     select
-        *,
-        -- Net Cash Flow
-        cash_from_operations + cash_from_investing + cash_from_financing as net_cash_flow
-    from cash_flow
+        {{ base_cols_sql }},
+        accounting_period_ending,
+        cash_flow_category,
+        cash_beginning_period,
+        cash_ending_period,
+        cash_ending_period - coalesce(cash_beginning_period, 0) as cash_net_period
+    from with_lag
 )
 
 select *
