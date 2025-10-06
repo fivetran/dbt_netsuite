2ed82cd2b848d62fd251df23ec3992fa51a3eba5
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2025-10-06 17:48:53 -0500

    update staging vars

--- models/netsuite2/staging/tmp/stg_netsuite2__accounting_book_subsidiaries_tmp.sql
+++ models/netsuite2/staging/tmp/stg_netsuite2__accounting_book_subsidiaries_tmp.sql
@@ -1,4 +1,12 @@
-{{ config(enabled=(var('netsuite_data_model', 'netsuite') == var('netsuite_data_model_override','netsuite2') and var('netsuite2__multibook_accounting_enabled', true))) }}
+{{
+    config(
+        enabled=(
+            var('netsuite_data_model', 'netsuite') == var('netsuite_data_model_override','netsuite2')
+            and var('netsuite2__multibook_accounting_enabled', true)
+            and var('netsuite2__using_accounting_book_subsidiaries', true)
+            )
+    )
+}}
 
 {{
     netsuite.union_netsuite_connections(
