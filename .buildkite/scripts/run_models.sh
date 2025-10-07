b1b63b409c8c6aba2bb9638aca1130829d781f30
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2025-10-07 10:08:16 -0500

    fix ymls

--- .buildkite/scripts/run_models.sh
+++ .buildkite/scripts/run_models.sh
@@ -19,18 +19,18 @@ cd integration_tests
 dbt deps
 
 if [ "$db" = "databricks-sql" ]; then
-dbt seed --vars '{netsuite_schema: netsuite_integrations_tests_sqlw}' --target "$db" --full-refresh
-dbt source freshness --vars '{netsuite_schema: netsuite_integrations_tests_sqlw}' --target "$db" || echo "...Only verifying freshness runs…"
-dbt compile --vars '{netsuite_schema: netsuite_integrations_tests_sqlw}' --target "$db"
-dbt run --vars '{netsuite_schema: netsuite_integrations_tests_sqlw}' --target "$db" --full-refresh
-dbt run --vars '{netsuite_schema: netsuite_integrations_tests_sqlw}' --target "$db"
-dbt test --vars '{netsuite_schema: netsuite_integrations_tests_sqlw}' --target "$db"
-dbt run --vars '{netsuite_schema: netsuite_integrations_tests_sqlw, netsuite2__using_to_subsidiary: true, netsuite2__multibook_accounting_enabled: true, netsuite2__using_exchange_rate: false, netsuite2__using_vendor_categories: false, netsuite2__using_jobs: false, netsuite2__using_employees: false, netsuite2__fiscal_calendar_enabled: true}' --target "$db" --full-refresh
-dbt run --vars '{netsuite_schema: netsuite_integrations_tests_sqlw, netsuite2__using_to_subsidiary: true, netsuite2__multibook_accounting_enabled: true, netsuite2__using_exchange_rate: false, netsuite2__using_vendor_categories: false, netsuite2__using_jobs: false, netsuite2__using_employees: false, netsuite2__fiscal_calendar_enabled: true}' --target "$db"
-dbt test --vars '{netsuite_schema: netsuite_integrations_tests_sqlw}' --target "$db"
-dbt run --vars '{netsuite_schema: netsuite_integrations_tests_sqlw, netsuite2__using_to_subsidiary: true, netsuite2__using_exchange_rate: true}' --target "$db" --full-refresh
-dbt run --vars '{netsuite_schema: netsuite_integrations_tests_sqlw, netsuite2__using_to_subsidiary: true, netsuite2__using_exchange_rate: true}' --target "$db"
-dbt test --vars '{netsuite_schema: netsuite_integrations_tests_sqlw}' --target "$db"
+dbt seed --vars '{netsuite_schema: netsuite_integrations_tests_sqlw_2}' --target "$db" --full-refresh
+dbt source freshness --vars '{netsuite_schema: netsuite_integrations_tests_sqlw_2}' --target "$db" || echo "...Only verifying freshness runs…"
+dbt compile --vars '{netsuite_schema: netsuite_integrations_tests_sqlw_2}' --target "$db"
+dbt run --vars '{netsuite_schema: netsuite_integrations_tests_sqlw_2}' --target "$db" --full-refresh
+dbt run --vars '{netsuite_schema: netsuite_integrations_tests_sqlw_2}' --target "$db"
+dbt test --vars '{netsuite_schema: netsuite_integrations_tests_sqlw_2}' --target "$db"
+dbt run --vars '{netsuite_schema: netsuite_integrations_tests_sqlw_2, netsuite2__using_to_subsidiary: true, netsuite2__multibook_accounting_enabled: true, netsuite2__using_exchange_rate: false, netsuite2__using_vendor_categories: false, netsuite2__using_jobs: false, netsuite2__using_employees: false, netsuite2__fiscal_calendar_enabled: true}' --target "$db" --full-refresh
+dbt run --vars '{netsuite_schema: netsuite_integrations_tests_sqlw_2, netsuite2__using_to_subsidiary: true, netsuite2__multibook_accounting_enabled: true, netsuite2__using_exchange_rate: false, netsuite2__using_vendor_categories: false, netsuite2__using_jobs: false, netsuite2__using_employees: false, netsuite2__fiscal_calendar_enabled: true}' --target "$db"
+dbt test --vars '{netsuite_schema: netsuite_integrations_tests_sqlw_2}' --target "$db"
+dbt run --vars '{netsuite_schema: netsuite_integrations_tests_sqlw_2, netsuite2__using_to_subsidiary: true, netsuite2__using_exchange_rate: true}' --target "$db" --full-refresh
+dbt run --vars '{netsuite_schema: netsuite_integrations_tests_sqlw_2, netsuite2__using_to_subsidiary: true, netsuite2__using_exchange_rate: true}' --target "$db"
+dbt test --vars '{netsuite_schema: netsuite_integrations_tests_sqlw_2}' --target "$db"
 
 else
 
